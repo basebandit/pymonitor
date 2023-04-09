33b1a59606faf762779114dcd1c9e4a6409c057d
basebandit <evansonmwangi83@gmail.com>
2023-04-09 12:14:35 +0300

    test the index render function

---
+++ tests/app_test.py
@@ -0,0 +1,25 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    """Create and configure a new app instance for each test."""
+    app = create_app({"TESTING": True})
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
+
+
+def test_index(client):
+    response = client.get("/")
+    print(f"response {response.data}")
+    assert b"System Monitor" in response.data
+    assert b"CPU Utilization" in response.data
+    assert b"Memory Utilization" in response.data
+    assert b'id="cpu-gauge"' in response.data
+    assert b'id="mem-gauge"' in response.data
