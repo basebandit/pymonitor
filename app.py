de9145214cdaa9a306466b9aa8f1875d6603b622
basebandit <evansonmwangi83@gmail.com>
2023-04-09 12:14:19 +0300

    refactor to allow for testing

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import psutil
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    if test_config:
+        app.config.update(test_config)
 
-@app.route("/")
-def index():
-    cpu_metric = psutil.cpu_percent()
-    mem_metric = psutil.virtual_memory().percent
+    @app.route("/")
+    def index():
+        cpu_metric = psutil.cpu_percent()
+        mem_metric = psutil.virtual_memory().percent
 
-    message = ""
-    if cpu_metric > 80 or mem_metric > 80:
-        message = "High CPU or Memory utilization detected. Please scale up."
-    return render_template(
-        "index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=message
-    )
+        message = ""
+        if cpu_metric > 80 or mem_metric > 80:
+            message = "High CPU or Memory utilization detected. Please scale up."
+        return render_template(
+            "index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=message
+        )
 
+    return app
